e9c192da166dca25ceb80fb637a24ae80dd45688
Kondwani David. Mwale <kondwanim@livestock.local>
2021-10-22 15:50:12 -0700

    additon of amended, applications and permits

--- helpers/mutation-types.js
+++ helpers/mutation-types.js
@@ -2,27 +2,62 @@
 // SHARED
 export const SET_LOADING = 'SET_LOADING'
 
+
+
+
 // Auth
 export const SET_AUTH_USER = 'SET_AUTH_USER'
 
-// USERS
+
+
+
+
+
+// -----------------------------------------USERS----------------------------------------------------------------------
 export const SET_USERS = 'SET_USERS'
 export const ADD_NEW_USER = 'ADD_NEW_USER'
 export const SET_CURRENT_USER = 'SET_CURRENT_USER'
 export const CACHE_USER = 'CACHE_USER'
 
-// PROCUREMENT
+//----------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+//--------------------------------------- PROCUREMENT------------------------------------------------------------------
 export const ADD_PFI = 'ADD_PFI'
 export const SET_ALL_PFIS = 'SET_ALL_PFIS'
 export const GET_ALL_PFIS = 'GET_ALL_PFIS'
 
+//----------------------------------------------------------------------------------------------------------------------
 
 
 
-// COMPLIANCE
+
+
+
+// ----------------------------------------COMPLIANCE---------------------------------------------------------------------
+
+//AMENDED PERMIT APPLICATIONS
 export const ADD_AMENDED_PERMIT_APPLICATION = 'ADD_AMENDED_PERMIT_APPLICATION'
 export const SET_ALL_AMENDED_PERMIT_APPLICATIONS = 'SET_ALL_AMENDED_PERMIT_APPLICATIONS'
 export const GET_ALL_AMENDED_PERMIT_APPLICATIONS = 'GET_ALL_AMENEDED_PERMIT_APPLICATIONS'
+
+//PERMIT APPLICATIONS
+export const ADD_PERMIT_APPLICATION = 'ADD_PERMIT_APPLICATION'
+export const SET_ALL_PERMIT_APPLICATIONS = 'SET_ALL_PERMIT_APPLICATIONS'
+export const GET_ALL_PERMIT_APPLICATIONS = 'GET_ALL_PERMIT_APPLICATIONS'
+
+//PERMITS
+export const ADD_PERMIT = 'ADD_PERMIT'
+export const SET_ALL_PERMITS = 'SET_ALL_PERMITS'
+export const GET_ALL_PERMITS = 'GET_ALL_PERMITS'
+
+//--------------------------------------------------------------------------------------------------------------------------
+
+
 
 
 
--- store/compliance.js
+++ store/compliance.js
@@ -7,22 +7,84 @@ import {
          SET_LOADING, 
          ADD_AMENDED_PERMIT_APPLICATION,
          SET_ALL_AMENDED_PERMIT_APPLICATIONS,
-         GET_ALL_AMENDED_PERMIT_APPLICATIONS
+         GET_ALL_AMENDED_PERMIT_APPLICATIONS,
+
+         ADD_PERMIT_APPLICATION,
+         SET_ALL_PERMIT_APPLICATIONS,
+         GET_ALL_PERMIT_APPLICATIONS,
+
+         ADD_PERMIT,
+         SET_ALL_PERMITS,
+         GET_ALL_PERMITS,
+         
+
         } from '@/helpers/mutation-types'
 
 
 export const state = () => ({
     loading: false,
-    all:[],
+
+
+    //--------------AMENDMENTS TO PERMIT APPLICATIONS------------
+    allAmendedPermitApplications:[],
     
-    form:{
+    amendmentForm:{
         
         pfiNumber:null,
         reasonForAmendment:null,
 
               
     },
+
+    //-------------------------------------------------------------
+
+
+
+     //--------------PERMIT APPLICATIONS---------------------------
+     allPermitApplications:[],
+    
+    permitApplicationForm:{
+        
+        pfiNumber:null,
+        authBody: null,
+        permitApplicationAmount:null,
+
+              
+    },
+     //-------------------------------------------------------------
+
+
+
+     //----------------PERMITS--------------------------------------
+     allPermits:[],
+    
+    permitForm:{
+        
+        pfiNumber:null,
+        reasonForAmendment:null,
+
+              
+    },
+     //-------------------------------------------------------------
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export const getters = {
     getField,
@@ -31,10 +93,45 @@ export const getters = {
         return state.loading
     },
 
+
+    //--------------AMENDMENTS TO PERMIT APPLICATIONS------------
+
     allAmendedPermitApplications(state){
-        return state.all
+        return state.allAmendedPermitApplications
     },
+
+    //-------------------------------------------------------------
+
+
+
+
+
+
+    //--------------PERMIT APPLICATIONS---------------------------
+    allPermitApplications(state){
+        return state.allPermitApplications
+    },
+    //-------------------------------------------------------------
+
+
+
+
+
+    //----------------PERMITS--------------------------------------
+    allPermits(state){
+        return state.allPermits
+    },
+    //-------------------------------------------------------------
 }
+
+
+
+
+
+
+
+
+
 
 export const mutations = {
     updateField,
@@ -43,26 +140,79 @@ export const mutations = {
         state.loading = loading
     },  
 
-    //MUTATIONS FOR ADDING, SETTING AND GETTING PFIs
+    //--------------AMENDMENTS TO PERMIT APPLICATIONS---------------------------------------------------
     [ADD_AMENDED_PERMIT_APPLICATION](state, newAmendedPermitApplication){
-        state.all.push(newAmendedPermitApplication)
+        state.allAmendedPermitApplications.push(newAmendedPermitApplication)
     },
     [SET_ALL_AMENDED_PERMIT_APPLICATIONS](state, payload) {
-        state.all = payload
+        state.allAmendedPermitApplications = payload
     },
     [GET_ALL_AMENDED_PERMIT_APPLICATIONS](state, payload){
-        state.all = payload
+        state.allAmendedPermitApplications = payload
     },
+
+     //--------------------------------------------------------------------------------------------------
+
+
+
+      //--------------PERMIT APPLICATIONS-----------------------------------------------------------------
+    [ADD_PERMIT_APPLICATION](state, newPermitApplication){
+        state.allPermitApplications.push(newPermitApplication)
+    },
+    [SET_ALL_PERMIT_APPLICATIONS](state, payload) {
+        state.allPermitApplications = payload
+    },
+    [GET_ALL_PERMIT_APPLICATIONS](state, payload){
+        state.allPermitApplications = payload
+    },
+
+     //---------------------------------------------------------------------------------------------------
+
+
+
+
+
+      //--------------PERMITS------------------------------------------------------------------------------
+    [ADD_PERMIT](state, newPermit){
+        state.allPermits.push(newPermit)
+    },
+    [SET_ALL_PERMITS](state, payload) {
+        state.allPermits = payload
+    },
+    [GET_ALL_PERMITS](state, payload){
+        state.allPermits = payload
+    },
+
+     //-----------------------------------------------------------------------------------------------------
+
+
+
 }
 
+
+
+
 export const actions = {
-      //LOAD ALL PFIS UPON REFRESH
+
+      //-------------------------------------------LOAD ALL RECORDS----------------------------------------------
       async load({ commit }) {
         try {
             commit(SET_LOADING, true)
-           const newLoad= await api.get(`/comp/permits/allAmendedPermitApplications`)
+
+
+            
+           const amendLoad= await api.get(`/comp/permits/allAmendedPermitApplications`)
            // await dispatch('getInactivePolicies')
-        console.log(newLoad.data)
+        console.log(amendLoad.data)
+
+        const permitApplicationsLoad= await api.get(`/comp/permits/allPermitApplications`)
+           // await dispatch('getInactivePolicies')
+        console.log(permitApplicationsLoad.data)
+
+
+        const permitsLoad= await api.get(`/comp/permits/allPermits`)
+           // await dispatch('getInactivePolicies')
+        console.log(permitsLoad.data)
 
          //GET ALL PFI RECORDS FROM API
          const {data: allPfis} = await api.get(`/pfis/AllPfis`)
@@ -70,7 +220,11 @@ export const actions = {
          console.log(allPfis);
 
          
-             commit(SET_ALL_AMENDED_PERMIT_APPLICATIONS, newLoad.data);
+             commit(SET_ALL_AMENDED_PERMIT_APPLICATIONS, amendLoad.data);
+
+             commit(SET_ALL_PERMIT_APPLICATIONS, permitApplicationsLoad.data);
+
+             commit(SET_ALL_PERMITS, permitsLoad.data);
             //   ...getters.allPolicies,
             //   ...getters.inactivePolicies,
             commit(SET_LOADING, false);
@@ -80,7 +234,21 @@ export const actions = {
             this.$log.error(error.message)
         }
     },
- //GET ALL PFIS
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+  //------------------------------ACTIONS FOR AMENDMENTS TO PERMIT APPLICATIONS-------------------------------------------------------------
+
     async getAllAmendedPermitApplications({ commit }){
         try {
             //ENABLE LOADING FEATURE WHILE API REQUEST IS BEING MADE
@@ -112,12 +280,12 @@ export const actions = {
         }
     },
 
-    //ADD NEW PFI TO ALL PFIs
+    //ADD NEW AMENDED PERMIT TO ALL AMENDED PERMIT APPLICATIONS
     async addNewAmendedPermitApplication({ commit, state}){
         try {
             commit(SET_LOADING, true);
 
-            const newAmendedPermitApplication = state.form;
+            const newAmendedPermitApplication = state.amendmentForm;
            
            
             const response = await api.post(`/comp/permits/addNewAmendedPermitApplication`, newAmendedPermitApplication);
@@ -132,6 +300,132 @@ export const actions = {
             commit(SET_LOADING, false);
             this.log.error(error.message);
         }
+    },
+
+ //-------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+    //--------------------------------------ACTIONS FOR PERMIT APPLICATIONS-------------------------------------------------------------------
+    async getAllPermitApplications({ commit }){
+        try {
+            //ENABLE LOADING FEATURE WHILE API REQUEST IS BEING MADE
+            commit(SET_LOADING, true)
+
+           
+            //API REQUEST IS MADE AND RESULT IS STORED IN CONST
+           const {data: allPermitApplications} = await api.get(`/comp/permits/allPermitApplications`)
+
+           
+
+           console.log(allPermitApplications);
+
+
+           //RETRIEVED DATA IS COMMITTED TO THE MUTATION TO MAKE THE CHANGES EFFECTIVE
+           commit(GET_ALL_PERMIT_APPLICATIONS, allPermitApplications.data);
+            
+           
+           //AFTER ALL ACTIONS HAVE BEEN PERFORMED, LOADING IS SET TO FALSE AND RESULTS ARE DISPLAYED
+           commit(SET_LOADING, false);
+
+        } catch (error) {
+            commit(SET_LOADING, false);
+            this.$log.error(error.message)
+        }
+    },
+
+    //ADD NEW PERMIT APPLCATION TO ALL PERMIT APPLICATIONS
+    async addNewPermitApplication({ commit, state}){
+        try {
+            commit(SET_LOADING, true);
+
+            const newPermitApplication = state.permitApplicationForm;
+           
+           
+            const response = await api.post(`/comp/permits/addNewPermitApplication`, newPermitApplication);
+
+            console.log(response.data);
+
+            commit(ADD_PERMIT_APPLICATION, response.data);
+            
+            commit(SET_LOADING, false);
+
+        } catch (error) {
+            commit(SET_LOADING, false);
+            this.log.error(error.message);
+        }
+    },
+     //---------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+    //---------------------------------------ACTIONS FOR PERMITS------------------------------------------------------------------------------
+    async getAllPermits({ commit }){
+        try {
+            //ENABLE LOADING FEATURE WHILE API REQUEST IS BEING MADE
+            commit(SET_LOADING, true)
+
+           
+            //API REQUEST IS MADE AND RESULT IS STORED IN CONST
+           const {data: allPermits} = await api.get(`/comp/permits/allPermits`)
+
+           
+
+           console.log(allPermits);
+
+
+           //RETRIEVED DATA IS COMMITTED TO THE MUTATION TO MAKE THE CHANGES EFFECTIVE
+           commit(GET_ALL_PERMITS, allPermits.data);
+            
+           
+           //AFTER ALL ACTIONS HAVE BEEN PERFORMED, LOADING IS SET TO FALSE AND RESULTS ARE DISPLAYED
+           commit(SET_LOADING, false);
+
+        } catch (error) {
+            commit(SET_LOADING, false);
+            this.$log.error(error.message)
+        }
+    },
+
+    //ADD NEW PERMIT TO ALL PERMITS
+    async addNewPermit({ commit, state}){
+        try {
+            commit(SET_LOADING, true);
+
+            const newPermit = state.permitForm;
+           
+           
+            const response = await api.post(`/comp/permits/addNewPermit`, newPermit);
+
+            console.log(response.data);
+
+            commit(ADD_PERMIT, response.data);
+            
+            commit(SET_LOADING, false);
+
+        } catch (error) {
+            commit(SET_LOADING, false);
+            this.log.error(error.message);
+        }
     }
+     //---------------------------------------------------------------------------------------------------------------------------------------
+
+
 }
 
