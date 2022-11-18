565aba3b96e097a856cd5925d3fd67536127732b
Kondwani D. Mwale <kondwanim@livestock.local>
2022-11-18 08:02:47 +0200

    calendar

---
+++ components/calendar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+        <ejs-schedule height="550px">
+
+
+        </ejs-schedule>
+  </div>
+</template>
+
+<script>
+
+import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda } from '@syncfusion/ej2-vue-schedule'
+
+export default {
+ components:{
+    'ejs-schedule': ScheduleComponent
+ },
+
+ provide:{
+    schedule: [ Day, Week, WorkWeek, Month, Agenda]
+ }
+}
+</script>
+
+<style>
+@import '../node_modules/@syncfusion/ej2-base/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-buttons/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-calendars/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-dropdowns/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-inputs/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-navigations/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-popups/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-vue-schedule/styles/material.css';
+</style>
